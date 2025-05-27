87afd46e94a01075984fbb621005776b80c1da99
benshabbat <benshabbat27@gmail.com>
2025-05-27 14:31:25 +0300

    implement twoSum and twoSum2 functions for finding indices of two numbers that add up to a target

---
+++ TwoSum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++)
+      if (nums[i] + nums[j] === target) return [i, j];
+  }
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum2 = function (nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i);
+  }
+};
+
+console.log(twoSum2([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum2([3, 2, 4], 6)); // [1, 2]
